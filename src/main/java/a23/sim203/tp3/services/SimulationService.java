d99e41265deb3cfeda400053ab0e682f4012ea71
Emile <a@a.com>
2023-11-13 13:49:18 -0500

    debut service

--- src/main/java/a23/sim203/tp3/services/SimulationService.java
+++ src/main/java/a23/sim203/tp3/services/SimulationService.java
@@ -1,30 +1,34 @@
 package a23.sim203.tp3.services;
 
 import a23.sim203.tp3.modele.MoteurCalcul;
+import javafx.concurrent.ScheduledService;
 import javafx.concurrent.Service;
 import javafx.concurrent.Task;
 
 import java.time.LocalDateTime;
 
-public class SimulationService extends Service<Void> {
+public class SimulationService extends ScheduledService<Void> {
     private MoteurCalcul moteurCalcul;
     private long startTime;
     private long endTime;
-    private long differenceTemps;
+    private long deltaTemps;
     private long intervalleDeTemps;
+
+    public void setDeltaTemps(long deltaTemps) {
+        this.deltaTemps = 3;
+    }
+
+    public void setStartTime(long startTime) {
+        this.startTime = System.currentTimeMillis();
+    }
 
     @Override
     protected Task<Void> createTask() {
         return new Task<Void>() {
             @Override
             protected Void call() throws Exception {
-                for (int i = 0; i < intervalleDeTemps; i++) {
-                    startTime = (System.currentTimeMillis() * 1000);
-
-
-                    endTime = (System.currentTimeMillis() * 1000);
-                    differenceTemps = endTime - startTime;
-                }
+                moteurCalcul.calcule("sim_");
+                moteurCalcul.avancePasDeTemps();
                 return null;
             }
         };
