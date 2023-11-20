5a5bed73d7177e094ee7218af658df8eb7cca9ed
JaykeGagne <126257547+JaykeGagne@users.noreply.github.com>
2023-11-20 09:18:26 -0500

    Programmation des boutons de l'interface

---
+++ src/main/java/a23/sim203/tp3/controller/TableauController.java
@@ -0,0 +1,29 @@
+/*
+
+Cours : 420-201 - Introduction a la programmation
+Groupe : 1
+Nom: Gagne
+Prenom: Jayke
+DA: 2264136
+*/
+
+package a23.sim203.tp3.controller;
+
+import a23.sim203.tp3.app.GestionAffichage;
+import a23.sim203.tp3.modele.MoteurCalcul;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class TableauController {
+
+    @FXML
+    private Button button;
+    MoteurCalcul moteurCalcul;
+
+    GestionAffichage gestionAffichage;
+
+}
