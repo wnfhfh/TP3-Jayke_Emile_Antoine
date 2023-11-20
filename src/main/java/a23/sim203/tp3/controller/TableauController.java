cb5c106b585dee69511773b615f0cedc01a72e0b
JaykeGagne <126257547+JaykeGagne@users.noreply.github.com>
2023-11-19 20:48:25 -0500

    Commencement du Controller pour le Tableau

---
+++ src/main/java/a23/sim203/tp3/controller/TableauController.java
@@ -0,0 +1,25 @@
+/*
+
+Cours : 420-201 - Introduction a la programmation
+Groupe : 1
+Nom: Gagne
+Prenom: Jayke
+DA: 2264136
+*/
+
+package a23.sim203.tp3.controller;
+
+import a23.sim203.tp3.app.GestionAffichage;
+import a23.sim203.tp3.modele.MoteurCalcul;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class TableauController {
+    @FXML
+    private Button button;
+    GestionAffichage gestionAffichage;
+    MoteurCalcul moteurCalcul;
+
+
+
+}
